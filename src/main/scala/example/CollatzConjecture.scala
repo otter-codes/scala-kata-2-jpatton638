e7c575a641809a6142dd4aac6e1218b18e50dbfd
jpatton638 <jpatton638@gmail.com>
2018-02-07 16:57:08 +0000

    Refactored to improve

--- src/main/scala/example/CollatzConjecture.scala
+++ src/main/scala/example/CollatzConjecture.scala
@@ -8,45 +8,39 @@ object CollatzConjecture extends scala.App {
   def calculate(number: Int): Int = {
 
     val runningTotal = ListBuffer[Int](number)
-
     count(number)
 
-    def count(input: Int): Int = {
+    def count(input: Int): Unit = {
 
-      if (input > 1) {
-          val even = input % 2 == 0
-          even match {
-            case true =>
-              val newNum = input / 2
-              runningTotal.append(newNum)
-              count(newNum)
-            case false =>
-              val newNum = (input * 3) + 1
-              runningTotal.append(newNum)
-              count(newNum)
-          }
+      if (input != 1) {
+
+        input % 2 == 0 match {
+
+          case true =>
+            val newNum = input / 2
+            runningTotal.append(newNum)
+            count(newNum)
+
+          case false =>
+            val newNum = (input * 3) + 1
+            runningTotal.append(newNum)
+            count(newNum)
+        }
       } else {
         val list = runningTotal.toList
         println(s"$number needs a Collatz Conjecture of ${list.length}")
         println(list)
-        input
       }
     }
+
     number
   }
 
-
-
   lazy val number = StdIn.readLine("Enter a number: ").toInt
-
-  if (number < 1) {
-    println("Idiot")
-  }
 
   try {
     calculate(number)
   } catch {
     case e: NumberFormatException => println("Not a number")
   }
-
 }
